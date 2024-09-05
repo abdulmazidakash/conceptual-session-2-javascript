0fc7651d927f6cece653eafa3369b5fce939a698
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-05 07:48:25 +0600

    javascript conceptual session video 2 complete

--- array.js
+++ array.js
@@ -1,1 +1,42 @@
+// const numbers = [12, 32, 32, 43, 35, 64];
 
+// console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[0]);
+// const x = numbers[3];
+// console.log(x);
+
+// console.log(numbers);
+// numbers.push(100);
+// numbers.pop();
+// numbers.pop();
+// const x = numbers.pop();
+// const y = numbers.pop();
+// console.log(numbers);
+// console.log(x);
+// console.log(y);
+
+// const fruits = ['aam', 'jaam', 'kathal'];
+// const flowers =  ['golap', 'sapla', 'joba', 'beli'];
+// const numbers = [12, 32, 23, 23];
+// const akakar = fruits.concat(flowers);
+// const akakar = flowers.concat(fruits);
+// console.log(fruits);
+// console.log(flowers);
+// console.log(akakar);
+// console.log(akakar[4]);
+// const find = fruits.indexOf('jaam');
+// console.log(find);
+// const find1 = fruits.indexOf('dragon');
+// console.log(find1);
+
+// const flowers =  ['golap', 'sapla', 'joba', 'beli'];
+// console.log(flowers.includes('krinichura'));
+// console.log(flowers.includes('golap'));
+
+// const fools = flowers.join('|');
+// console.log(typeof fools);
+// console.log(Array.isArray(flowers));
+
+const fools = 'golam,shapla,joba,beli';
+const convert = String.split('');
